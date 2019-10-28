f3baa04c9bd596de8b7da43cc60cef3d81a5647f
Mohammed  Abdulaziz <mohammedazizbuss@gmail.com>
2019-10-28 00:12:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+let h1=document.createElement("h1");
+h1.id="victory";
+document.body.appendChild(h1);
+h1.innerHTML="Mohammed Abdulaziz is the champion";
