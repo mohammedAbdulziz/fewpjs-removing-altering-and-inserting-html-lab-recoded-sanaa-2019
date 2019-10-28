1925b58c03644fec311bf08e1bde016158c8ad9f
Mohammed  Abdulaziz <mohammedazizbuss@gmail.com>
2019-10-28 00:15:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+let newHeader=document.createElement("h1");
+newHeader.id="victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML="Mohammed Abdulaziz is the champion";
